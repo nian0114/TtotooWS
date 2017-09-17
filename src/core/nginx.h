703a7f9e1e9af49444c48188c52ffe9cf914c879
nian0114 <268078545@qq.com>
2017-09-17 23:39:50 +0800

    change nginx name

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -13,13 +13,9 @@
 #define NGINX_VERSION      "1.0.0"
 #define NGINX_VER          "ttotoo/" NGINX_VERSION
 
-#ifdef NGX_BUILD
-#define NGINX_VER_BUILD    NGINX_VER " (" NGX_BUILD ")"
-#else
-#define NGINX_VER_BUILD    NGINX_VER
-#endif
+#define NGINX_VER_BUILD    "ttotoo"
 
-#define NGINX_VAR          "NGINX"
+#define NGINX_VAR          "ttotoo"
 #define NGX_OLDPID_EXT     ".oldbin"
 
 
